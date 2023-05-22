fa9f16970b4fc3b988f3bfb9856236eabf7b545a
Amaototi <taiki.ymd@gmail.com>
2023-05-22 18:25:22 +0900

    Fix nest

--- main.go
+++ main.go
@@ -26,36 +26,36 @@ func main() {
 func runMarkov() {
 	var text string
 	check := regexp.MustCompile(`^http`)
+	markovBlock := [][]string{}
+	notes, err := misskeyapi.MisskeyGetnotesRequest(config.Loadconfig.Misskey.Token)
+	if err != nil {
+		return
+	}
+	m, err := mecab.New("-Owakati")
+	if err != nil {
+		log.Fatal("New mecab error: ", err)
+	}
+	defer m.Destroy()
+
+	for _, note := range notes {
+		_data := markov.ParseToNode(m, note)
+		elems := markov.GetMarkovBlocks(_data)
+		markovBlock = append(markovBlock, elems...)
+	}
+
+	noteElemset := markov.MarkovChainExec(markovBlock)
+
 	for {
-		markovBlock := [][]string{}
-		notes, err := misskeyapi.MisskeyGetnotesRequest(config.Loadconfig.Misskey.Token)
-		if err != nil {
-			return
-		}
-		m, err := mecab.New("-Owakati")
-		if err != nil {
-			log.Fatal("New mecab error: ", err)
-		}
-		defer m.Destroy()
-
-		for _, note := range notes {
-			_data := markov.ParseToNode(m, note)
-			elems := markov.GetMarkovBlocks(_data)
-			markovBlock = append(markovBlock, elems...)
-		}
-
-		noteElemset := markov.MarkovChainExec(markovBlock)
 		text = markov.TextGenerate(noteElemset)
 
 		if !check.MatchString(text) {
 			break
 		}
-
 	}
 
-	log.Print(text)
-	err := misskeyapi.MisskeySendnotesRequest(config.Loadconfig.Misskey.Token, text)
+	err = misskeyapi.MisskeySendnotesRequest(config.Loadconfig.Misskey.Token, text)
 	if err != nil {
+		log.Print(text)
 		return
 	}
 }
